98e99f042d9afa45c6f463ccce1729a5949ad6e8
Nikodem Reszka <61233315+nikodemrx@users.noreply.github.com>
2022-01-28 21:23:38 +0100

    v1.0.0 finished?

--- main.py
+++ main.py
@@ -8,27 +8,49 @@ specialchars = [chr(i) for i in range(33, 48)] + [chr(i) for i in range(58, 65)]
 largeletters = [chr(i) for i in range(65, 91)]
 smallletters = [chr(i) for i in range(97, 123)]
 
+options = [numbers, specialchars, largeletters, smallletters]
 current = 0
 selections = ["", "", "", ""]
+do_again = True
 
 
-def changeCurrent(dir):
+def underline(*text):
     global current
-    if dir and (current < 3):
+    text = list(text)
+    for i in range(len(text)):
+        if i == current:
+            text[i] = f'\033[4m{text[i]}\033[0m'
+    return text
+
+
+def changeCurrent(dirr):
+    global current
+    if dirr and (current < 3):
         current += 1
-    if (not dir) and (current != 0):
+    if (not dirr) and (current != 0):
         current -= 1
 
 
 def showSelection():
     global current
-    print("Select password types:")
-    print(f'[{selections[0]}] Numbers: {numbers}')
-    print(f'[{selections[0]}] Specials: [\'' + "\', \'".join(specialchars) + '\']')
-    print(f'[{selections[0]}] Large letters: {largeletters}')
-    print(f'[{selections[0]}] Small letters: {smallletters}')
+    print("Select password sing groups using arrows and X, press ENTER to submit, select nothing to select all types:")
+    for i in underline(f'[{selections[0]}] Numbers',
+                       f'[{selections[1]}] Specials',
+                       f'[{selections[2]}] Large letters',
+                       f'[{selections[3]}] Small letters'):
+        print(i)
     print(selections)
-    print(current)
+
+
+def get_selected():
+    to_use = []
+    for i in range(len(selections)):
+        if selections[i] == "X":
+            to_use += options[i]
+    if len(to_use) == 0:
+        for i in range(len(selections)):
+            to_use += options[i]
+    return to_use
 
 
 def on_press(key):
@@ -44,25 +66,32 @@ def on_release(key):
     if key == keyboard.Key.esc:
         # Stop listener
         return False
-    if str(key) == "Key.down":  # if key 'r' is pressed
+    elif str(key) == "Key.down":  # if key 'r' is pressed
         changeCurrent(True)
+        system("cls")
+        showSelection()
 
-    if str(key) == "Key.up":
+    elif str(key) == "Key.up":
         changeCurrent(False)
+        system("cls")
+        showSelection()
 
-    if "\'x\'" == str(key):
+    elif "\'x\'" == str(key):
         if selections[current] == "":
             selections[current] = "X"
         else:
             selections[current] = ""
         system("cls")
         showSelection()
-    else:
-        print(f'Key released: {key}')
+    elif "Key.enter" == str(key):
+        return False
+    # else:
+    #     print(f'Key released: {key}')
 
 
 def main():
     global current
+    global do_again
     system("cls")
     showSelection()
     with keyboard.Listener(
@@ -70,6 +99,21 @@ def main():
             on_release=on_release) as listener:
         listener.join()
 
+    input()
+    password_len = 0
+    while True:
+        try:
+            system('cls')
+            showSelection()
+            password_len = int(input("Enter number of words to be used in your password: "))
+            break
+        except ValueError:
+            continue
+    print("".join([choice(get_selected()) for _ in range(password_len)]))
+    if input("Want to get new password? Yes<X> | No: ").lower() != "no":
+        do_again = False
+
 
 if __name__ == '__main__':
-    main()
+    while do_again:
+        main()
