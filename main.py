fa2936846a427625f09448c9a6a8aa9408f0f467
Nikodem Reszka <61233315+nikodemrx@users.noreply.github.com>
2022-01-28 19:42:59 +0100

    v0.0.1 testing arrow gui navigation

--- main.py
+++ main.py
@@ -1,26 +1,75 @@
 from random import choice
-
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pynput import keyboard
+from os import system
 
 numbers = [str(i) for i in range(0, 10)]
-specialchars = [chr(i) for i in range(33, 48)] + [chr(i) for i in range(58, 65)] + [chr(i) for i in range(91, 97)] + [chr(i) for i in range(123, 127)]
+specialchars = [chr(i) for i in range(33, 48)] + [chr(i) for i in range(58, 65)] + [chr(i) for i in range(91, 97)] + [
+    chr(i) for i in range(123, 127)]
 largeletters = [chr(i) for i in range(65, 91)]
 smallletters = [chr(i) for i in range(97, 123)]
 
+current = 0
+selections = ["", "", "", ""]
+
+
+def changeCurrent(dir):
+    global current
+    if dir and (current < 3):
+        current += 1
+    if not dir and (current > 1):
+        current -= 1
+
+
+def showSelection():
+    global current
+    print("Select password types:")
+    print(f'[{selections[0]}] Numbers: {numbers}')
+    print(f'[{selections[0]}] Specials: [\'' + "\', \'".join(specialchars) + '\']')
+    print(f'[{selections[0]}] Large letters: {largeletters}')
+    print(f'[{selections[0]}] Small letters: {smallletters}')
+    print(selections)
+    print(current)
+
+
+def on_press(key):
+    try:  # used try so that if user pressed other than the given key error will not be shown
+        pass
+
+    except AttributeError:
+        print('special key pressed: {0}'.format(
+            key))
+
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+    if key == keyboard.Key.down:  # if key 'r' is pressed
+        changeCurrent(True)
+
+    if key == keyboard.Key.up:
+        changeCurrent(False)
+
+    if "x" == str(key):
+        if selections[current] == "":
+            selections[current] = "X"
+        else:
+            selections[current] = ""
+        system("cls")
+        showSelection()
+    else:
+        print('Key released: {0}'.format(
+            key))
 
 def main():
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'NUMS: {numbers}')
-    print(f'specials: [\'' + "\', \'".join(specialchars) + '\']')
-    print(f'large letters: {largeletters}')
-    print(f'small letters: {smallletters}')
+    global current
+    system("cls")
+    showSelection()
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
